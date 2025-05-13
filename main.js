c64a2f35d648173eaa6def6d5fc0109703ac0f37
Ubaidullah-M <ubaidullahmahmad@gmail.com>
2025-05-14 00:02:17 +0100

    integrated the backend code

--- main.js
+++ main.js
@@ -3,12 +3,22 @@ let close = document.querySelector('#close-menu');
 let nav = document.querySelector('#nav');
 let lists = document.querySelector('header ul');
 let navLinks = document.querySelector('#nav-links').querySelectorAll('li');
-let copyRight = document.querySelector("#copy-right")
+let copyRight = document.querySelector("#copy-right");
+let loader = document.querySelector(".my-loading")
+let responseTheme = document.querySelector(".message-note")
+let message = document.querySelector("#note")
+let closeBtn = document.querySelector("#close-message")
+const myForm = document.querySelector("#my-form")
+const animationCycle = document.querySelector(".load-cycle")
+let projectBtns = document.querySelectorAll("#portfolio-btn button")
+let data // this will be assigned value after form submission as a response
+
 
 // Toggling humberger menu on and off
-
 copyRight.innerText = new Date().getFullYear()
 
+
+// opening & closing humberger menu
 menu.addEventListener('click', () => {
     nav.classList.toggle("hidden")
 })
@@ -18,8 +28,8 @@ close.addEventListener('click', () => {
 
 })
 
-// Removing nav when clicking menu on small screen.
 
+// Removing nav when clicking menu on small screen.
 navLinks.forEach((nav) => {
     nav.addEventListener("click", closesMenu)
 })
@@ -28,8 +38,62 @@ function closesMenu() {
     nav.classList.toggle("hidden")
 }
 
-// About me section
 
+// fetching data $ sending a form data to database
+async function submitData() {
+    const ourPromise = await fetch("https://portfolio-serve-uei3.onrender.com/contact", {
+        method: "post",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: document.querySelector("#name").value,
+            email: document.querySelector("#email").value,
+            message: document.querySelector("#message").value
+        })
+    })
+    const ourData = await ourPromise.json()
+    data = await ourData
+}
+
+
+// form submission events
+myForm.addEventListener('submit', async function(e) {
+    e.preventDefault()
+    loader.classList.toggle("hidden")
+    await submitData()
+    if (data.success) {
+        animationCycle.classList.toggle("hidden")
+        responseTheme.classList.toggle("hidden")
+        message.innerText = `${data.message}`
+    
+    } else {
+        animationCycle.classList.toggle("hidden")
+        responseTheme.classList.toggle("hidden")
+        message.innerText = `${data.message}`
+    }
+})
+
+
+// closing the form submission wait
+closeBtn.addEventListener("click", function() {
+    loader.classList.toggle("hidden")
+    animationCycle.classList.toggle("hidden")
+     responseTheme.classList.toggle("hidden")
+     if (data.success) {
+        myForm.reset()
+        document.querySelector("#name").focus()
+     }
+})
+
+// changing the project buttons color
+projectBtns.forEach((btn) => {
+    btn.addEventListener('click', e => {
+        projectBtns.forEach(b => b.classList.remove("active"))
+        btn.classList.add("active")
+    })
+})
+
+
+// About me section
 let readMore = document.querySelector('#read-more');
 let about = document.querySelector('#about')
 
